b3d4bedf7c557b168b7cca80d5aad55b24af0602
OyarzoGabriel <gabriel.oyarzo@alu.frlp.utn.edu.ar>
2025-08-22 18:45:02 -0300

    Realizacion de Model Usuario y Persona, realizacion de routas para dashboards

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use App\Models\User;
+use App\Models\Usuario;
+use App\Models\Persona;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
@@ -34,11 +35,23 @@ class AuthController extends Controller
         if (Auth::attempt($credentials, $remember)) {
             $request->session()->regenerate();
             
+            $user = Auth::user();
             // Agregar el rol del usuario a la sesión
-            $request->session()->put('user_role', Auth::user()->rol);
+            $request->session()->put('rol', $user->rol);
             
-            return redirect()->intended(route('home'))->with('success', 'Bienvenido de vuelta!');
-        }
+            switch ($user->rol) {
+                case Usuario::ROL_ADMIN:
+                    return redirect()->route('admin.dashboard');
+                case Usuario::ROL_TECNICO:
+                    return redirect()->route('tecnico.dashboard');
+                case Usuario::ROL_ENCARGADO:
+                    return redirect()->route('encargado.dashboard');
+                case Usuario::ROL_AUDITOR:
+                    return redirect()->route('auditor.dashboard');
+                default:
+                    return redirect()->route('home');
+    }        
+           }
 
         throw ValidationException::withMessages([
             'email' => ['Las credenciales proporcionadas no coinciden con nuestros registros.'],
@@ -59,26 +72,28 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $request->validate([
-            'username'=>'required',
-            'name' => 'required|string|max:255',
-            'surname'=>'required',
-            'email' => 'required|string|email|max:255|unique:users',
+            'email' => 'required|string|email|max:255|unique:usuarios,email',
+            'rol' => 'required',
             'password' => 'required|string|min:8|confirmed',
+            'nombre'=>'required',
+            'apellido'=>'required',
 
         ]);
-
-        $user = User::create([
-            'name' => $request->name,
+        $usuario = Usuario::create([
             'email' => $request->email,
-            'password' => Hash::make($request->password),
-            'rol' => 'user', // Asignar rol por defecto
+            'password' => $request->password, //se encripta sola
+            'rol' => $request->rol,
+        ]);
+        $usuario->persona()->create([
+            'nombre' => $request->nombre,
+            'apellido' => $request->apellido,
+
         ]);
 
-        Auth::login($user);
 
+        Auth::login($usuario);
         // Agregar el rol del usuario a la sesión
-        $request->session()->put('user_role', $user->rol);
-
+        $request->session()->put('rol', $usuario->rol);
         return redirect(route('home'))->with('success', '¡Cuenta creada exitosamente!');
     }
 
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login')->middleware('guest');
-Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register')->middleware('guest');
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login')->middleware('guest'); // Mostrar formulario login
+Route::post('/login', [AuthController::class, 'login'])->middleware('guest'); // Procesar login
+
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register')->middleware('guest'); // Mostrar formulario registro
+Route::post('/register', [AuthController::class, 'register'])->middleware('guest');  // Procesar registro
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');  // Cerrar sesión
+
+//ROLES RUTAS PRINCIPALES
+    Route::get('/admin', function(){ return view('admin.dashboard'); })->name('admin.dashboard');
+    Route::get('/tecnico', function(){ return view('tecnico.dashboard'); })->name('tecnico.dashboard');
+    Route::get('/encargado', function(){ return view('encargado.dashboard'); })->name('encargado.dashboard');
+    Route::get('/auditor', function(){ return view('auditor.dashboard'); })->name('auditor.dashboard');
+
