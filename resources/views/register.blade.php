0c373f2d6bb1313e4441936ef5b2a14ac278fa4d
OyarzoGabriel <gabriel.oyarzo@alu.frlp.utn.edu.ar>
2025-08-22 23:18:58 -0300

    registro actualizado

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -21,14 +21,21 @@
   <!-- Formulario centrado -->
 <div class="registration-container">
   <div class="registration-form">
-    <h2 class="text-center mb-4">Ingresar Datos
-    </h2> <!-- Título agregado -->
-    <form>
-      <input type="text" class="form-control mb-2" placeholder="Nombre de usuario" required>
-      <input type="text" class="form-control mb-2" placeholder="Nombre" required>
-      <input type="text" class="form-control mb-2" placeholder="Apellido" required>
-      <input type="email" class="form-control mb-2" placeholder="Correo electrónico" required>
-      <input type="password" class="form-control mb-2" placeholder="Contraseña" required>
+    <h2 class="h2">Ingresar Datos</h2>
+    <form method="POST" action="{{route('register')}}">
+      @csrf
+      <input type="email" name="email" class="form-control mb-2" placeholder="Correo electrónico" required>
+      <input type="password" name="password" class="form-control mb-2" placeholder="Contraseña" required>
+      <input type="password" name="password_confirmation" class="form-control mb-2" placeholder="Confirmar Contraseña" required>
+      <input type="text" name="nombre" class="form-control mb-2" placeholder="Nombre" required>
+      <input type="text" name="apellido" class="form-control mb-2" placeholder="Apellido" required>
+      <label for="opciones">Rol del Usuario:</label>
+      <select name="rol" class="form-select form-select-sm-2">
+      <option value="administrador">Administrador</option>
+      <option value="tecnico">Tecnico</option>
+      <option value="encargado">Encargado</option>
+      <option value="auditor">Auditor</option>
+      </select>
       <button type="submit" class="registration-btn">Registrarse</button>
     </form>
   </div>
