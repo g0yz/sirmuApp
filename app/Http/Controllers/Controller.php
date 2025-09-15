905d3e521f987f5cc1b2f9bb41c81bd7f2ac0a59
OyarzoGabriel <gabriel.oyarzo@alu.frlp.utn.edu.ar>
2025-09-15 14:52:51 -0300

    instalacion de spatie media library

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 /**
 * @OA\Info(
-*             title="API Sede", 
+*             title="API Sirmu", 
 *             version="1.0",
-*             description="Listado de las URL de la Api Sede"
+*             description="Listado de las URL de la Api para el proyecto Sirmu para la materia de Desarollo de Software"
 * )
 *
 * @OA\Server(url="http://127.0.0.1:8000")
